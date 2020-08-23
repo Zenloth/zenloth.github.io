68ec9f970cd0f04acfc88ee3d550cbca3ae4628b
Zenloth <70053863+Zenloth@users.noreply.github.com>
2020-08-23 21:17:52 +0100

    Artnet calculator works. No validation.

--- js/artnet.js
+++ js/artnet.js
@@ -5,20 +5,22 @@ var dmxChans
 
 //Decimal Base 1 Trigger
 function decB1() {
+  decimalBase1 = Number(document.getElementById("ARTCalc-Decimal-B1").value) ;
   doMath();
 }
 //ARTNET Trigger
 function AN(){
+  decimalBase1 = parseInt(document.getElementById("ARTCalc-ARTNet").value,16);
   doMath();
 }
 //Decimal Base 0 Trigger
 function decB0() {
-  decimalBase1 = document.getElementByID("ARTCalc-Decimal-B0").value + 1;
+  decimalBase1 = Number(document.getElementById("ARTCalc-Decimal-B0").value) + 1;
   doMath();
 }
 //Decimal DMX Channel Trigger
 function DMXChan() {
-  decimalBase1 = ceil(document.getElementByID("ARTCalc-DMXChannels").value / 512);
+  decimalBase1 = Math.ceil(Number(document.getElementById("ARTCalc-DMXChannels").value) / 512);
   doMath();
 }
 
@@ -27,6 +29,7 @@ function DMXChan() {
 function doMath() {
   //Calculate ARTNet
   function calculateArtNet() {
+    artnet = decimalBase1.toString(16);
   }
 
   //Calculate Decimal universe (base0)
@@ -39,8 +42,15 @@ function doMath() {
 
   }
   function printANResults(){
-    document.getElementByID("ARTCalc-Decimal-B1").value = decimalBase1;
-    document.getElementByID("ARTCalc-Decimal-B0").value = decimalBase0;
-    document.getElementByID("ARTCalc-DMXChannels").value = dmxChans;
+    document.getElementById("ARTCalc-Decimal-B1").value = decimalBase1;
+    document.getElementById("ARTCalc-ARTNet").value = artnet;
+    document.getElementById("ARTCalc-Decimal-B0").value = decimalBase0;
+    document.getElementById("ARTCalc-DMXChannels").value = dmxChans;
   }
+
+  calculateArtNet();
+  calculateDecimalB0();
+  calculateMaxDMXChannels();
+  printANResults();
+
 };
