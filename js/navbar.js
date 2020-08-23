3158228712545cfa93b6bd47e04163c8525b0ef7
Zenloth <70053863+Zenloth@users.noreply.github.com>
2020-08-23 22:44:22 +0100

    Navbar fun.

--- js/navbar.js
+++ js/navbar.js
@@ -5,11 +5,13 @@ function youScrollMeRightRound() {
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
     document.getElementById("topnav").style.width = "110vw";
     document.getElementById("topnav").style.height = "6vh";
+    document.getElementById("topnav-container").style.height = "6vh";
     document.getElementById("topnav").style.margin = "0 -5% auto -5%";
 
   } else {
     document.getElementById("topnav").style.width = "80vw";
     document.getElementById("topnav").style.height = "10vh";
+    document.getElementById("topnav-container").style.height = "13vh";
     document.getElementById("topnav").style.margin = "3vh auto auto auto";
   }
 }
