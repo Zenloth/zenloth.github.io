d0d2f10703ebaf9ed6fb7ab90ef0ffd15f39ea54
Zenloth <70053863+Zenloth@users.noreply.github.com>
2020-08-23 21:57:45 +0100

    Some validation for artnet calc.

--- js/artnet.js
+++ js/artnet.js
@@ -5,23 +5,59 @@ var dmxChans
 
 //Decimal Base 1 Trigger
 function decB1() {
-  decimalBase1 = Number(document.getElementById("ARTCalc-Decimal-B1").value) ;
-  doMath();
+  //Validation
+    //Put whatever shit they've typed into the variable
+  decimalBase1 = document.getElementById("ARTCalc-Decimal-B1").value;
+    //If what they've typed isn't a number OR <1 OR >32,768 ragequit.
+    //To add - get rid of decimals.
+  if (isNaN(decimalBase1) || decimalBase1 < 1 || decimalBase1 > 32768 ) {
+    alert("A standard 'Universe Number' should be a whole number between 1 and 32,768 (the limit of Art-Net 3).")
+    return;
+  } else {
+  //Do the thing of making decimal value for calculations
+    decimalBase1 = Number(decimalBase1) ;
+    doMath();
+  }
+
 }
 //ARTNET Trigger
 function AN(){
+  //Validation
+  //Do the thing of making Decimal value
   decimalBase1 = parseInt(document.getElementById("ARTCalc-ARTNet").value,16);
   doMath();
 }
 //Decimal Base 0 Trigger
 function decB0() {
-  decimalBase1 = Number(document.getElementById("ARTCalc-Decimal-B0").value) + 1;
+  //Validation
+    //Put whatever shit they've typed into the variable
+    decimalBase0 = document.getElementById("ARTCalc-Decimal-B0").value
+    //If what they've typed isn't a number OR <0 OR >32,767 ragequit.
+    //To add - get rid of decimals.
+  if (isNaN(decimalBase0) || decimalBase0 < 0 || decimalBase0 > 32767 ) {
+    alert("A standard 'Base 0 Universe Number' should be a whole number between 0 and 32,767 (the limit of Art-Net 3).")
+    return;
+  } else{
+  //Do the thing of making Decimal value
+  decimalBase1 = Number(decimalBase0) + 1;
   doMath();
+  }
 }
 //Decimal DMX Channel Trigger
 function DMXChan() {
-  decimalBase1 = Math.ceil(Number(document.getElementById("ARTCalc-DMXChannels").value) / 512);
+  //Validation
+    //Put whatever shit they've typed into the variable
+    dmxChans = document.getElementById("ARTCalc-DMXChannels").value
+    //If what they've typed isn't a number OR <0 OR >32,767 ragequit.
+    //To add - get rid of decimals.
+    if (isNaN(decimalBase0) || decimalBase0 < 0 || decimalBase0 > (32768 * 512) ) {
+      alert("This calculator supports DMX Channel amounts of whole numbers between 0 and 16,777,216 (the limit of Art-Net 3).")
+      return;
+    } else{
+  //Do the thing of making Decimal value
+  decimalBase1 = Math.ceil(dmxChans).value) / 512);
   doMath();
+  }
 }
 
 
@@ -41,13 +77,14 @@ function doMath() {
     dmxChans = decimalBase1 * 512;
 
   }
+  //Output the values
   function printANResults(){
     document.getElementById("ARTCalc-Decimal-B1").value = decimalBase1;
     document.getElementById("ARTCalc-ARTNet").value = artnet;
     document.getElementById("ARTCalc-Decimal-B0").value = decimalBase0;
     document.getElementById("ARTCalc-DMXChannels").value = dmxChans;
   }
-
+  //Run the functions
   calculateArtNet();
   calculateDecimalB0();
   calculateMaxDMXChannels();
