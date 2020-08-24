f9aad75d995c101e65af0211c53ee5680b5e48d4
Zenloth <70053863+Zenloth@users.noreply.github.com>
2020-08-24 12:39:22 +0100

    Made navbar fixed and fixed some centering problems.

--- js/navbar.js
+++ js/navbar.js
@@ -3,15 +3,15 @@ window.onscroll = function () {youScrollMeRightRound()};
 function youScrollMeRightRound() {
   //if user scrolls more than 60px from the top
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
-    document.getElementById("topnav").style.width = "110vw";
-    document.getElementById("topnav").style.height = "6vh";
-    document.getElementById("topnav-container").style.height = "6vh";
-    document.getElementById("topnav").style.margin = "0 -5% auto -5%";
+    document.getElementById("topnav").style.width = "100vw";
+    document.getElementById("topnav").style.height = "3vh";
+    document.getElementById("topnav-container").style.height = "3vh";
+    document.getElementById("topnav").style.marginTop = "0";
 
   } else {
     document.getElementById("topnav").style.width = "80vw";
     document.getElementById("topnav").style.height = "10vh";
     document.getElementById("topnav-container").style.height = "13vh";
-    document.getElementById("topnav").style.margin = "3vh auto auto auto";
+    document.getElementById("topnav").style.marginTop = "3vh";
   }
 }
