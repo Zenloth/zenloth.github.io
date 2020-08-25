e960a7228cca8c88c3edaf78c5816da2a0ff0b38
Zenloth <70053863+Zenloth@users.noreply.github.com>
2020-08-25 11:47:35 +0100

    Fixing navbar centering, added some min heights etc.

--- js/navbar.js
+++ js/navbar.js
@@ -3,15 +3,17 @@ window.onscroll = function () {youScrollMeRightRound()};
 function youScrollMeRightRound() {
   //if user scrolls more than 60px from the top
   if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60){
-    document.getElementById("topnav").style.width = "100vw";
-    document.getElementById("topnav").style.height = "3vh";
-    document.getElementById("topnav-container").style.height = "3vh";
+    document.getElementById("topnav").style.width = "103vw";
+    document.getElementById("topnav").style.height = "5vh";
+    document.getElementById("topnav-container").style.height = "5vh";
+    document.getElementById("topnav-container").style.minHeight = "5vh";
     document.getElementById("topnav").style.marginTop = "0";
 
   } else {
     document.getElementById("topnav").style.width = "80vw";
     document.getElementById("topnav").style.height = "10vh";
     document.getElementById("topnav-container").style.height = "13vh";
+    document.getElementById("topnav-container").style.minHeight = "70px";
     document.getElementById("topnav").style.marginTop = "3vh";
   }
 }
